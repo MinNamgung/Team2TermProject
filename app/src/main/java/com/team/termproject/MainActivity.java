965c5eba71979e026c332faca7a9b365b5980c6b
Aaron Jordan <aaronjordan4.aj@gmail.com>
2019-04-20 18:58:39 -0400

    Updated MainActivity to link login button to List activity. Added ListActivity.class, Subscription.class, SubscriptionListAdapter.class, activity_list.xml, adapter_view_layout.xml

--- app/src/main/java/com/team/termproject/MainActivity.java
+++ app/src/main/java/com/team/termproject/MainActivity.java
@@ -1,13 +1,31 @@
 package com.team.termproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.loginBtn);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openListActivity();
+            }
+        });
+    }
+
+    public void openListActivity(){
+        Intent intent = new Intent(MainActivity.this, ListActivity.class);
+        startActivity(intent);
     }
 }
